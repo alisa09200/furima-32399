054193a0fc63337d6a7fb0d573cf20d410689391
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-02-17 17:33:25 +0900

    カラムの修正と追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_014142) do
+ActiveRecord::Schema.define(version: 2021_02_17_083231) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -21,14 +21,16 @@ ActiveRecord::Schema.define(version: 2021_02_17_014142) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
-    t.string "name", null: false
-    t.string "namekana", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
     t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name_kana"
+    t.string "first_name_kana"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
