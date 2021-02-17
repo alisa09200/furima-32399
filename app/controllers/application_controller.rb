e9892645f0bbad35c9a899e79eb6c44fb3b01f2f
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-02-17 18:31:37 +0900

    ストロングパラメーター修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name, :namekana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :birthday, :last_name_kana, :first_name_kana])
   end
 end
