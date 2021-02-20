c77dc958fc5063f669101aae4ff2cd1abe995a7a
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-02-20 21:36:48 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   validates :nickname, presence: true
   validates :last_name, presence: true
+  validates :last_name, format:{with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
   validates :first_name, presence: true
+  validates :first_name, format:{with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
   validates :birthday, presence: true
   validates :last_name_kana, presence: true
+  validates :last_name_kana, format:{with: /\A[ァ-ヶ]+\z/}
   validates :first_name_kana, presence: true
+  validates :first_name_kana, format:{with: /\A[ァ-ヶ]+\z/}
+  validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
 end
