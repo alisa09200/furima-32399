bf4b6eae36796a627aeba5f0a9c862a0c51aa8a4
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-02-23 13:19:31 +0900

    itemsテーブルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  include ActiveHash::Associations
+  has_many :areas
+  has_many :categorys
+  has_many :condition
+  has_many :periods
+  has_many :postage
 end
