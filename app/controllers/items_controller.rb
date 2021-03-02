0e355db4e761674ea462b916d644e5d937b5829b
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-03-02 18:24:06 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.order(id: "DESC")
@@ -19,13 +21,30 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:product, :explanation, :category_id, :condition_id, :postage_id, :area_id, :period_id, :price,
-                                 :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product, :explanation, :category_id, :condition_id, :postage_id, :area_id, :period_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 end
