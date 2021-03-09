c82f8e0b5d6db511b8347a3d47b4e535b6478ad4
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-03-09 12:58:20 +0900

    購入後、編集・購入ボタンを非表示にする実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -56,6 +56,6 @@ class ItemsController < ApplicationController
   end
 
   def contributor_confirmation
-    redirect_to root_path unless current_user.id == @item.user.id
+    redirect_to root_path unless current_user.id == @item.user.id || @item.purchase != nil
   end
 end
