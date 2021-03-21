72e1db8f34dc335c8d9587cabd2cd4049614f6cc
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-03-21 12:19:11 +0900

    マイページにカード情報を表示する実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
-  def show
-  end
 
+  def show
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    card = Card.find_by(user_id: current_user.id)
+ 
+    redirect_to new_card_path and return unless card.present?
+ 
+    customer = Payjp::Customer.retrieve(card.customer_token)
+    @card = customer.cards.first
+  end
+ 
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -9,10 +17,11 @@ class UsersController < ApplicationController
       redirect_to "show"
     end
   end
-
+ 
   private
-
+ 
   def user_params
-    params.require(:user).permit(:email, :nickname)
+    params.require(:user).permit(:name, :email)
   end
-end
+ 
+ end
