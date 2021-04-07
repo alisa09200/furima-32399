2e9952cf689912f1d38b81cb9ea6a2e1e5e1f052
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-04-07 12:41:59 +0900

    ルーティング記載

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :cards, only: [:new, :create]
   resources :items do
     resources :purchases, only: [:index, :create]
+    resources :comments, only: :create
     #post 'order', on: :member
   end
 end
