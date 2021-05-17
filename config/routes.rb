39c22ed1f6248a30bfc73ee332e1f19f3db75e2f
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-05-17 13:24:56 +0900

    ルーティング追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+   }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   get 'cards/new'
   get 'users/show'
-  devise_for :users
   root to: 'items#index'
   resources :users, only: [:show, :update]
   resources :cards, only: [:new, :create]
