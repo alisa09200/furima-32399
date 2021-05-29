35e0ad7c53e93d15c4084712d42fcbc0448d12e9
alisa09200 <alisa_yamazaki_1119@yahoo.co.jp>
2021-05-29 09:27:24 +0900

    保存先をlocalに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ApplicationRecord
 
   def self.guest
     find_or_create_by(email: "test@com") do |user|
-      user.password = 123abc
+      user.password = 1234567
     end
   end
 
